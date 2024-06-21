807f6b05a856040d396db54ff1d4100bf7bc0966
palagosm <palagosm@duocuc.cl>
2024-06-21 17:36:46 -0400

    Fixed typo in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-
-set -0 errexit
+#!/usr/bin/env bash
+set -o errexit
 
 pip install -r requirements.txt
-
-python manage.py collecstatic --no-input
+python manage.py collectstatic --no-input
 python manage.py migrate
